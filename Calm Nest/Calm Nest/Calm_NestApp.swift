b65d967f8f1ebd227fee4058e84c4ad08840c48d
Pratik Ghadge <pratikhg.2001@gmail.com>
2025-05-06 00:45:48 +0530

    Project Init

---
+++ Calm Nest/Calm Nest/Calm_NestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calm_NestApp.swift
+//  Calm Nest
+//
+//  Created by Pratik Ghadge on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Calm_NestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
